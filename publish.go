e5893eecddb09ebb626d02f9f99a5e7ec8cf6d2f
Holzer <holzebrt@schaeffler.com>
2020-01-07 20:34:05 +0100

    change publish type

--- publish.go
+++ publish.go
@@ -7,23 +7,27 @@ import (
 
 //Publish test
 type Publish struct {
-	Command string
-	Topic   string
-	Message []byte
+	Command     string
+	Topic       string
+	Message     []byte
+	CompleteMsg []byte
 }
 
 //HandlePublish is cool
-func HandlePublish(b []byte, mqttLen int) (Publish, error) {
+func HandlePublish(completeB []byte, header int, mqttLen int) (Publish, error) {
+
 	p := Publish{Command: "Publish"}
+	b := completeB[header:]
 
 	topic, len := getUtf8(b)
 	p.Topic = topic
 	log.Printf("topic: %s %d\n", topic, len)
 
-	pos := len + 2
+	pos := len + 1
 	payLen := mqttLen - pos
 	msg := b[pos : pos+payLen]
 	p.Message = msg
+	p.CompleteMsg = completeB[:mqttLen+2]
 	fmt.Printf("pub payload: %s\n", string(msg))
 
 	return p, nil
